40b72df586d76939f0bd6d27125423f3643f5aba
Austen Bruhn <=>
2024-08-06 14:29:31 -0600

    remove more deprecations

--- banner.py
+++ banner.py
@@ -6,44 +6,36 @@ from Xlib import X
 
 def on_configure_event(window, event, data):
     # Adjust the window properties when resized or moved
-    screen = window.get_screen()
-    monitors = []
-    nmons = screen.get_n_monitors()
-    for m in range(nmons):
-        mg = screen.get_monitor_geometry(m)
-        monitors.append(mg)
-
     display = Display()
     topw = display.create_resource_object('window',
                                           window.get_toplevel().get_window().get_xid())
 
-    main_monitor = screen.get_primary_monitor()
+    monitor = Gdk.Display.get_default().get_primary_monitor()
+    geometry = monitor.get_geometry()
 
-    for i, monitor in enumerate(monitors):
-        x = monitor.x
-        y = monitor.y
-        width = monitor.width
-        height = monitor.height
+    x = geometry.x
+    y = geometry.y
+    width = geometry.width
+    height = geometry.height
 
-        # Adjust y position to be below the status bar only for the main monitor
-        status_bar_height = 24  # Adjust this value as needed
-        if i == main_monitor:
-            y += status_bar_height
+    # Adjust y position to be below the status bar only for the primary monitor
+    status_bar_height = 24  # Adjust this value as needed
+    y += status_bar_height
 
-        # Move and resize the window
-        window.move(x, y)
-        window.resize(width, data['bar_size'])
+    # Move and resize the window
+    window.move(x, y)
+    window.resize(width, data['bar_size'])
 
-        # Reserve space (a "strut") for the bar
-        strut_top = data['bar_size'] + (status_bar_height if i == main_monitor else 0)
-        topw.change_property(display.intern_atom('_NET_WM_STRUT'),
-                             display.intern_atom('CARDINAL'), 32,
-                             [0, 0, strut_top, 0],
-                             X.PropModeReplace)
-        topw.change_property(display.intern_atom('_NET_WM_STRUT_PARTIAL'),
-                             display.intern_atom('CARDINAL'), 32,
-                             [0, 0, strut_top, 0, 0, 0, 0, 0, x, x + width - 1, 0, 0],
-                             X.PropModeReplace)
+    # Reserve space (a "strut") for the bar
+    strut_top = data['bar_size'] + status_bar_height
+    topw.change_property(display.intern_atom('_NET_WM_STRUT'),
+                         display.intern_atom('CARDINAL'), 32,
+                         [0, 0, strut_top, 0],
+                         X.PropModeReplace)
+    topw.change_property(display.intern_atom('_NET_WM_STRUT_PARTIAL'),
+                         display.intern_atom('CARDINAL'), 32,
+                         [0, 0, strut_top, 0, 0, 0, 0, 0, x, x + width - 1, 0, 0],
+                         X.PropModeReplace)
 
 def main():
     print("Gtk %d.%d.%d" % (Gtk.get_major_version(),
@@ -75,15 +67,15 @@ def main():
     }
     """)
     Gtk.StyleContext.add_provider_for_screen(
-        Gdk.Screen.get_default(),
+        Gdk.Display.get_default().get_default_screen(),
         style_provider,
         Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 
     # (b) Add a label to the center of the bar
-    center_label = Gtk.Label(
+    center_label = Gtk.Label()
+    center_label.set_markup(
         "<span font_family='%s' weight='%s' foreground='%s' size='%s'>%s</span>" % (
             font, weight, fgcolor, size, message))
-    center_label.set_use_markup(True)
     center_label.set_justify(Gtk.Justification.CENTER)
     center_label.set_yalign(0.5)
     center_label.set_xalign(0.5)
